c0b9fddafa24f3672f5a37d73e288e68e581f177
Kelli Horacek <kelliscoding@gmail.com>
2023-04-07 10:44:11 -0500

    Complete the Advanced Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(myArray){
+   return myArray.slice(0,2); 
+}
+
+const returnLastTwoDrivers = function(myArray){
+    return myArray.slice(-2, myArray.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier};
+}
+
+const fareDoubler = function (fare){
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function (fare){
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(myArray, fnctn){
+    return fnctn(myArray);
+}
